fadf5dacd5b8837f1d14e87526347d61c4d42f75
Nkenna Aniedobe <nkenna@fb.com>
2018-07-09 15:59:17 -0700

    Initial Commit

---
+++ Instayam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instayam
+//
+//  Created by Nkenna Aniedobe on 7/9/18.
+//  Copyright © 2018 Nkenna Aniedobe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Instayam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instayam
+//
+//  Created by Nkenna Aniedobe on 7/9/18.
+//  Copyright © 2018 Nkenna Aniedobe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
